59b1fe14ee5d1c849d8c64459ee0c5367ccde6ee
Duncan Zauss <duncan.zauss@gmx.net>
2021-08-26 11:11:26 +0200

    Added torchhub downloadable ShuffleNetV2K16 checkpoint

--- openpifpaf_mpii/__init__.py
+++ openpifpaf_mpii/__init__.py
@@ -5,3 +5,6 @@ from .mpii import MPII
 
 def register():
     openpifpaf.DATAMODULES['mpii'] = MPII
+    openpifpaf.CHECKPOINT_URLS['shufflenetv2k16-mpii'] = 'https://github.com/DuncanZauss/' \
+        'openpifpaf_mpii/releases/download/v.0.1.0-alpha/mpii_sk16.pkl.epoch350'
+
